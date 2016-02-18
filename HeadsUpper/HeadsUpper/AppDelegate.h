1f2d412c967686c05b2ca0588b57543bcdc3add1
mikekavouras <kavourasm@gmail.com>
2016-02-17 23:51:55 -0500

    empty project and topics

---
+++ HeadsUpper/HeadsUpper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeadsUpper
+//
+//  Created by Michael Kavouras on 2/17/16.
+//  Copyright © 2016 Michael Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
