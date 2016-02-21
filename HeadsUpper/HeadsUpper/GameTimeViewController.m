6708d1d2f216a6ca560a2cf824b0fe5f9b9a641c
shenayoshida <sheyoshida@gmail.com>
2016-02-21 17:42:55 -0500

    here it is

--- HeadsUpper/HeadsUpper/GameTimeViewController.m
+++ HeadsUpper/HeadsUpper/GameTimeViewController.m
@@ -27,14 +27,10 @@
     [super viewDidLoad];
     
     self.view.layer.backgroundColor = [UIColor lightGrayColor].CGColor;
-
     self.navigationController.navigationBar.topItem.title = @"Menu";
-
     
     self.questionCount = 1;
-    
     self.topicLabel.text = [self.category.items objectAtIndex:0];
-   
     self.score = 0;
     
     [self setupTimer];
@@ -45,11 +41,9 @@
 - (void)setupTimer {
     
     self.questionCount ++;
-    
     self.timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
     
     [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
-    
     self.timerCount = 10;
     
     [self.timer fire]; // start timer
@@ -57,18 +51,17 @@
 
 - (void)timerFired:(NSTimer *)timer {
     
-     NSString *time = [[NSNumber numberWithInteger:self.timerCount]stringValue];
+    NSString *time = [[NSNumber numberWithInteger:self.timerCount]stringValue];
     
     if (self.timerCount == 0) {
-        
         [self swipeLeft];
     }
-    
     self.timerCount --;
     self.timerLabel.text = time;
 }
 
 - (void)setupGestureRecognizers {
+    
     UISwipeGestureRecognizer *leftSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeLeft)];
     leftSwipe.direction = UISwipeGestureRecognizerDirectionLeft;
     
@@ -77,7 +70,7 @@
     
     [self.view addGestureRecognizer:leftSwipe];
     [self.view addGestureRecognizer:rightSwipe];
-
+    
 }
 
 // left = wrong
@@ -93,7 +86,7 @@
         [self setupTimer];
         
         self.topicLabel.text = [self.category.items objectAtIndex:self.questionCount];
-   
+        
     } else {
         self.view.layer.backgroundColor = [UIColor lightGrayColor].CGColor;
         [self.timer invalidate];
@@ -110,29 +103,24 @@
     [UIView animateWithDuration:0.25 animations:^{
         self.view.layer.backgroundColor = [UIColor greenColor].CGColor;
     } completion:nil];
-
-    if (self.questionCount != self.category.items.count - 1) {
     
-    [self.timer invalidate];
-    [self setupTimer];
+    if (self.questionCount != self.category.items.count - 1) {
         
-    self.score ++;
-    self.topicLabel.text = [self.category.items objectAtIndex:self.questionCount];
-    NSLog(@"score: %ld", (long)self.score);
+        [self.timer invalidate];
+        [self setupTimer];
+        self.score ++;
+        self.topicLabel.text = [self.category.items objectAtIndex:self.questionCount];
+        NSLog(@"score: %ld", (long)self.score);
         
     } else {
-    self.view.layer.backgroundColor = [UIColor lightGrayColor].CGColor;
-    [self.timer invalidate];
-    NSString *score = [[NSNumber numberWithInteger:self.score]stringValue];
-    NSString *numberOfQuestions = [[NSNumber numberWithInteger:self.questionCount]stringValue];
-    self.topicLabel.text = [NSString stringWithFormat:@"YOUR SCORE: \n%@/%@", score, numberOfQuestions];
-    self.timerLabel.hidden = YES;
+        
+        self.view.layer.backgroundColor = [UIColor lightGrayColor].CGColor;
+        [self.timer invalidate];
+        NSString *score = [[NSNumber numberWithInteger:self.score]stringValue];
+        NSString *numberOfQuestions = [[NSNumber numberWithInteger:self.questionCount]stringValue];
+        self.topicLabel.text = [NSString stringWithFormat:@"YOUR SCORE: \n%@/%@", score, numberOfQuestions];
+        self.timerLabel.hidden = YES;
     }
 }
-
-
-
-
-
 
 @end
