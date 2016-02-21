ce1f5a8809257f907de603ce5c27a1a63ae0fef0
ayunav <ayunanyc@gmail.com>
2016-02-21 17:11:21 -0500

    finished the heads up game logic. table vc UI is left.

---
+++ HeadsUpGameApp/AVHeadsUpGame/AVHeadsUpGame/AVHUGCluesViewController.h
@@ -0,0 +1,15 @@
+//
+//  AVHUGCluesViewController.h
+//  AVHeadsUpGame
+//
+//  Created by Ayuna Vogel on 2/21/16.
+//  Copyright © 2016 Ayuna Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVHUGCluesViewController : UIViewController
+
+@property (nonatomic) NSDictionary *category; 
+
+@end
---
+++ HeadsUpGameApp/AVHeadsUpGame/AVHeadsUpGame/AVHUGObject.m
@@ -0,0 +1,13 @@
+//
+//  AVHUGObject.m
+//  AVHeadsUpGame
+//
+//  Created by Ayuna Vogel on 2/21/16.
+//  Copyright © 2016 Ayuna Vogel. All rights reserved.
+//
+
+#import "AVHUGObject.h"
+
+@implementation AVHUGObject
+
+@end
