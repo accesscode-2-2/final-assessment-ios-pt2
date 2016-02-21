f850b211df8ad3b9e0ad5beb5ca0781d5d2e062f
Henna <henna.ahmed92@gmail.com>
2016-02-21 13:03:56 -0500

    required stuff done

--- HeadsUpper/HeadsUpper/ViewController.m
+++ HeadsUpper/HeadsUpper/ViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ViewController.h"
+#import <ChameleonFramework/Chameleon.h>
+#import <SIAlertView/SIAlertView.h>
 
 @interface ViewController ()
+@property (nonatomic) NSInteger timerCount;
+@property (nonatomic) NSInteger readyTimerCount;
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *guess;
+@property (nonatomic) NSInteger rightCount;
+@property (weak, nonatomic) IBOutlet UIView *readyView;
+@property (weak, nonatomic) IBOutlet UILabel *readyLabel;
+
+@property (nonatomic) NSInteger whichAnswerAreWeAt;
+@property (nonatomic) NSInteger totalGuesses;
 
 @end
 
@@ -16,7 +28,145 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.whichAnswerAreWeAt = 0;
+    self.rightCount = 0;
+    self.totalGuesses = 1;
+    self.guess.text = self.answers[self.whichAnswerAreWeAt];
+    self.readyLabel.text = @"3";
+    [self setupReadyTimer];
+    self.readyView.backgroundColor = [UIColor colorWithGradientStyle:UIGradientStyleTopToBottom withFrame: self.view.frame andColors:@[[UIColor flatWhiteColorDark], [UIColor flatWhiteColor]]];
+    
+}
+
+# pragma mark - gesture methods
+- (void) setUpGestureRecognizers {
+    UISwipeGestureRecognizer *leftSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    leftSwipe.direction= UISwipeGestureRecognizerDirectionLeft;
+    
+    UISwipeGestureRecognizer *rightSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    rightSwipe.direction = UISwipeGestureRecognizerDirectionRight;
+    
+    
+    
+    [self.view addGestureRecognizer:leftSwipe];
+    [self.view addGestureRecognizer:rightSwipe];
+    
+}
+
+- (void) handleSwipe: (UISwipeGestureRecognizer *) gesture {
+    switch (gesture.direction) {
+        case  UISwipeGestureRecognizerDirectionRight:
+            [self animateViewWith:[UIColor flatWatermelonColorDark]];
+            self.whichAnswerAreWeAt++;
+            self.totalGuesses++;
+            self.guess.text = self.answers[self.whichAnswerAreWeAt];
+            
+            break;
+        case UISwipeGestureRecognizerDirectionLeft:
+            [self animateViewWith:[UIColor flatMintColorDark]];
+            self.rightCount++;
+            self.totalGuesses++;
+            self.whichAnswerAreWeAt++;
+            self.guess.text = self.answers[self.whichAnswerAreWeAt];
+            break;
+        default:
+            break;
+    }
+}
+
+-(void) animateViewWith:(UIColor* )color{
+    [UIView animateWithDuration:0.5
+                     animations:^{
+                         self.view.backgroundColor = color;
+                     } completion:^(BOOL finished) {
+                         self.view.backgroundColor = [UIColor whiteColor];
+                     }];
+}
+
+# pragma mark - timer methods
+-(void) setupReadyTimer
+{
+    self.readyTimerCount = 2;
+    NSTimer *timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(handleReadyTimer:) userInfo:nil repeats:YES];
+    
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+    
+}
+
+- (void) handleReadyTimer: (NSTimer * ) timer {
+    
+    
+    self.readyLabel.text = [NSString stringWithFormat: @"%ld", (long)self.readyTimerCount];
+    self.readyTimerCount--;
+    
+    if (self.readyTimerCount < 0 ) {
+        self.readyLabel.text = @"Go!";
+        [UIView animateWithDuration:1.0
+                         animations:^{
+                             self.readyView.transform = CGAffineTransformMakeScale(1.5, 1.5);
+                         } completion:^(BOOL finished) {
+                             self.readyView.hidden = YES;
+                         }];
+        
+        [self setupTimer];
+        [timer invalidate];
+        //hide ready screen
+    }
+    
+}
+
+-(void) setupTimer
+{
+    self.timerCount = 10;
+    NSTimer *timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(handleGameTimer:) userInfo:nil repeats:YES];
+    
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+    //[timer fire];
+    [self setUpGestureRecognizers];
+}
+
+- (void) handleGameTimer: (NSTimer * ) timer {
+    
+    
+    self.timerLabel.text = [NSString stringWithFormat: @"%ld", (long)self.timerCount];
+    self.timerCount--;
+    
+    if (self.timerCount < 0 ) {
+        [timer invalidate];
+        
+        //show score
+        NSString * endGameText = [NSString stringWithFormat:@"you got %ld out of %ld right", (long)self.rightCount, self.totalGuesses];
+        SIAlertView *alertView = [[SIAlertView alloc] initWithTitle:@"Game Over" andMessage:endGameText];
+        
+        [alertView addButtonWithTitle:@"Play Again"
+                                 type:SIAlertViewButtonTypeDefault
+                              handler:^(SIAlertView *alert) {
+                                  
+                              }];
+        
+//        alertView.willShowHandler = ^(SIAlertView *alertView) {
+//            NSLog(@"%@, willShowHandler", alertView);
+//        };
+//        alertView.didShowHandler = ^(SIAlertView *alertView) {
+//            NSLog(@"%@, didShowHandler", alertView);
+//        };
+//        alertView.willDismissHandler = ^(SIAlertView *alertView) {
+//            NSLog(@"%@, willDismissHandler", alertView);
+//        };
+        alertView.didDismissHandler = ^(SIAlertView *alertView) {
+            [self dismissViewControllerAnimated:YES completion:nil];
+        };
+        
+        alertView.transitionStyle = SIAlertViewTransitionStyleBounce;
+        
+        [alertView show];
+        
+        
+        //end game
+        
+        //
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
