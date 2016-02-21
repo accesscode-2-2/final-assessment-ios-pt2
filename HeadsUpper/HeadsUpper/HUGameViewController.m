0bb96e1e28c2426f7088b3b18133c32ad6bdbebe
Mesfin Bekele Mekonnen <mesbekmek@gmail.com>
2016-02-21 12:55:09 -0500

    Cleaner GameViewController code

--- HeadsUpper/HeadsUpper/HUGameViewController.m
+++ HeadsUpper/HeadsUpper/HUGameViewController.m
@@ -7,23 +7,43 @@
 //
 
 #import "HUGameViewController.h"
+#import "HUGameCategory+GameScore.h"
 
 @interface HUGameViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *clueLabel;
 
-@property (nonatomic) NSInteger *gameScore;
+@property (nonatomic) NSInteger gameScore;
+@property (nonatomic) NSInteger currentIndex;
 
 @end
 
 @implementation HUGameViewController
 
+#pragma mark - Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.clueLabel.text =self.category.clues[0];
+    [self setup];
     [self setupSwipeGestures];
 }
+
+#pragma mark - setup
+
+- (void)setup {
+    self.gameScore = 0;
+    self.currentIndex = 0;
+    self.clueLabel.text =self.category.clues[self.currentIndex];
+}
+
+#pragma mark - Label Helper Method
+
+-(void)setupNextClue {
+    self.currentIndex+=1;
+    self.clueLabel.text = self.category.clues[self.currentIndex];
+}
+
+#pragma mark - Swipes
 
 - (void)setupSwipeGestures {
     
@@ -42,12 +62,39 @@
 }
 
 - (void)handleSwipeLeft:(UISwipeGestureRecognizer *)gesture {
-    
-    self.view.backgroundColor = [UIColor orangeColor];
+    if (self.currentIndex < self.category.clues.count-1) {
+        [self setupNextClue];
+        self.view.backgroundColor = [UIColor orangeColor];
+    }
+    else {
+        [self showAlert];
+    }
 }
 
 - (void)handleSwipeRight:(UISwipeGestureRecognizer *)gesture {
-    self.view.backgroundColor = [UIColor greenColor];
+    if (self.currentIndex < self.category.clues.count-1) {
+        [self setupNextClue];
+        self.view.backgroundColor = [UIColor greenColor];
+        self.gameScore+=1;
+    }
+    else {
+        [self showAlert];
+    }
 }
+
+#pragma mark - Alert
+
+-(void)showAlert {
+    NSString *score = [HUGameCategory getreportForScore:self.gameScore andCategory:self.category];
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Game Over" message:[NSString stringWithFormat:@"You got: %@",score] preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
 
 @end
