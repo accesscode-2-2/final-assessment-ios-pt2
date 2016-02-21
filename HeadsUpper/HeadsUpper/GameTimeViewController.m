87b3bed0e825b6a01c1ac311bf7fbfe880f467c6
shenayoshida <sheyoshida@gmail.com>
2016-02-21 14:31:46 -0500

    error handlig and display score

--- HeadsUpper/HeadsUpper/GameTimeViewController.m
+++ HeadsUpper/HeadsUpper/GameTimeViewController.m
@@ -14,6 +14,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *topicLabel;
 @property (weak, nonatomic) IBOutlet UILabel *timerLabel;
 
+@property (nonatomic) NSTimer *timer;
 @property (nonatomic) NSInteger timerCount;
 @property (nonatomic) NSInteger questionCount;
 @property (nonatomic) NSInteger score;
@@ -25,9 +26,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    self.topicLabel.text = [self.category.items objectAtIndex:0];
     
+    self.questionCount = 1;
+    
+    self.topicLabel.text = [self.category.items objectAtIndex:0];
+   
     self.score = 0;
     
     [self setupTimer];
@@ -37,13 +40,15 @@
 
 - (void)setupTimer
 {
-    NSTimer *timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
+    self.questionCount ++;
     
-    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+    self.timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
+    
+    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
     
     self.timerCount = 10;
     
-    [timer fire]; // start timer
+    [self.timer fire]; // start timer
 }
 
 - (void)timerFired:(NSTimer *)timer {
@@ -51,14 +56,8 @@
      NSString *time = [[NSNumber numberWithInteger:self.timerCount]stringValue];
     
     if (self.timerCount < 1) {
-        [timer invalidate];
         
-        // here's where we will bump to next question
-        [self advanceQuestion];
-        
-        // reset timer and label:
-        self.timerCount = 0;
-        self.timerLabel.text = time;
+        [self swipeLeft];
     }
     
     self.timerCount --;
@@ -66,11 +65,10 @@
 }
 
 - (void)setupGestureRecognizers {
-    
-    UISwipeGestureRecognizer *leftSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(advanceQuestion)];
+    UISwipeGestureRecognizer *leftSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeLeft)];
     leftSwipe.direction = UISwipeGestureRecognizerDirectionLeft;
     
-    UISwipeGestureRecognizer *rightSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(advanceQuestion)];
+    UISwipeGestureRecognizer *rightSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeRight)];
     rightSwipe.direction = UISwipeGestureRecognizerDirectionRight;
     
     [self.view addGestureRecognizer:leftSwipe];
@@ -79,10 +77,50 @@
 }
 
 - (void)advanceQuestion {
-    self.questionCount ++;
-    self.topicLabel.text = [self.category.items objectAtIndex:self.questionCount];
+
 }
 
+// left = wrong
+- (void)swipeLeft {
+    
+    if (self.questionCount != self.category.items.count - 1) {
+        
+        [self.timer invalidate];
+        [self setupTimer];
+        
+        self.topicLabel.text = [self.category.items objectAtIndex:self.questionCount];
+   
+    } else {
+        
+        [self.timer invalidate];
+        NSString *score = [[NSNumber numberWithInteger:self.score]stringValue];
+        NSString *numberOfQuestions = [[NSNumber numberWithInteger:self.questionCount]stringValue];
+        self.topicLabel.text = [NSString stringWithFormat:@"YOUR SCORE: \n%@/%@", score, numberOfQuestions];
+        self.timerLabel.hidden = YES;
+    }
+}
+
+// right = correct
+- (void)swipeRight {
+
+    if (self.questionCount != self.category.items.count - 1) {
+    
+    [self.timer invalidate];
+    [self setupTimer];
+        
+    self.score ++;
+    self.topicLabel.text = [self.category.items objectAtIndex:self.questionCount];
+    NSLog(@"score: %ld", (long)self.score);
+        
+    } else {
+        
+    [self.timer invalidate];
+    NSString *score = [[NSNumber numberWithInteger:self.score]stringValue];
+    NSString *numberOfQuestions = [[NSNumber numberWithInteger:self.questionCount]stringValue];
+    self.topicLabel.text = [NSString stringWithFormat:@"YOUR SCORE: \n%@/%@", score, numberOfQuestions];
+    self.timerLabel.hidden = YES;
+    }
+}
 
 
 
