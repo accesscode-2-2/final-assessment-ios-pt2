dda51d04143a856dc6c5d671d89fe9afdfd1f085
Daniel Distant <daniel.distant1@gmail.com>
2016-02-21 14:59:19 -0500

    first commit, basic game mechanics finished

---
+++ HeadsUpper/HUViewController.h
@@ -0,0 +1,15 @@
+//
+//  HUViewController.h
+//  HeadsUpper
+//
+//  Created by Daniel Distant on 2/21/16.
+//  Copyright © 2016 Michael Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HUViewController : UIViewController
+
+@property NSArray *answers;
+
+@end
---
+++ HeadsUpper/HeadsUpper/HUTopics.h
@@ -0,0 +1,15 @@
+//
+//  HUTopics.h
+//  HeadsUpper
+//
+//  Created by Daniel Distant on 2/21/16.
+//  Copyright © 2016 Michael Kavouras. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HUTopics : NSObject
+
+@property NSDictionary *topics;
+
+@end
