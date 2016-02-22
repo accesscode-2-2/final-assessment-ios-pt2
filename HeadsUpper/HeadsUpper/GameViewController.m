06bb7a11414c40cbd531b84e847b97fc7c5ca835
Brian <brianblanco1@gmail.comm>
2016-02-22 01:27:47 -0500

    commit

--- HeadsUpper/HeadsUpper/GameViewController.m
+++ HeadsUpper/HeadsUpper/GameViewController.m
@@ -12,6 +12,9 @@
 @interface GameViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *gameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (nonatomic) NSUInteger count;
+@property (nonatomic) NSTimer *countDown;
 
 @end
 
@@ -23,19 +26,46 @@
     
    // self.gameLabel.text = self.category;
     [self randomClue];
+    [self timerCountDown];
+    
 }
 
 -(void) randomClue
 {
-    NSUInteger suffleSubjects = arc4random_uniform(self.data.subject.count);
+    NSUInteger shuffleSubjects = arc4random_uniform(self.data.subject.count);
     
-    NSString *shuffledSubject = [self.data.subject objectAtIndex: suffleSubjects];
+    NSString *shuffledSubject = [self.data.subject objectAtIndex: shuffleSubjects];
     
     self.gameLabel.text = shuffledSubject;
     
     
 }
 
+-(void) timerCountDown
+{
+    self.count = 20;
+    self.countDown = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(startCounting) userInfo:nil repeats:YES];
+}
+
+-(void)startCounting
+{
+    self.count--;
+    self.timerLabel.text = [NSString stringWithFormat:@"%i", self.count];
+    
+    if (self.count == 0) {
+        [self.countDown invalidate];
+        
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@"Game Over"
+                              message:nil
+                              delegate:self
+                              cancelButtonTitle:@"Done"
+                              otherButtonTitles:nil];
+        
+        [alert show];
+    }
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
